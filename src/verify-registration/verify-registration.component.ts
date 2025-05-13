78ed59fcce2d8fdb1778e4d4de17f87059fa8b85
GianmariaZorzetto <gianzorzi2001@gmail.com>
2025-05-13 09:51:26 +0200

    introdotto component verifica registrazione

---
+++ src/verify-registration/verify-registration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-registration',
+  imports: [],
+  templateUrl: './verify-registration.component.html',
+  styleUrl: './verify-registration.component.css'
+})
+export class VerifyRegistrationComponent {
+
+}
