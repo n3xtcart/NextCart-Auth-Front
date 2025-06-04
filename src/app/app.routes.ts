2b8f5026a62f0aa3a101223d87cdd6fe5b4700de
GianmariaZorzetto <gianzorzi2001@gmail.com>
2025-06-04 15:16:14 +0200

    formattazione migliorata

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,8 @@ import {MainMenuComponent} from '../main-menu/main-menu.component';
 import {MainMenuShowGroupsComponent} from '../main-menu/main-menu-show-groups/main-menu-show-groups.component';
 import {MainMenuShowRolesComponent} from '../main-menu/main-menu-show-roles/main-menu-show-roles.component';
 import {MainMenuShowUsersComponent} from '../main-menu/main-menu-show-users/main-menu-show-users.component';
-import { CreateUserComponent } from './create-user/create-user.component';
-import { AuthGuard } from './AuthGurd';
+import {CreateUserComponent} from './create-user/create-user.component';
+import {AuthGuard} from './AuthGurd';
 
 export const routes: Routes = [
   {
@@ -20,23 +20,32 @@ export const routes: Routes = [
   },
   {
     path: 'verify',
-    component: VerifyRegistrationComponent, canActivate:[AuthGuard]
+    component: VerifyRegistrationComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'main-menu',
-    component: MainMenuComponent, canActivate:[AuthGuard]
+    component: MainMenuComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'main-menu/groups',
-    component: MainMenuShowGroupsComponent, canActivate:[AuthGuard]
+    component: MainMenuShowGroupsComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'main-menu/roles',
-    component: MainMenuShowRolesComponent, canActivate:[AuthGuard]
+    component: MainMenuShowRolesComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'main-menu/users',
-    component: MainMenuShowUsersComponent, canActivate:[AuthGuard]
+    component: MainMenuShowUsersComponent,
+    canActivate: [AuthGuard]
   },
-  {path:"createUser",component:CreateUserComponent, canActivate:[AuthGuard]}
+  {
+    path: "createUser",
+    component: CreateUserComponent,
+    canActivate: [AuthGuard]
+  }
 ];
